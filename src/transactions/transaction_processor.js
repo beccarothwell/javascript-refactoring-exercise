d5ac3c3c51b540e99635bca47193213d693ab118
Becca Rothwell <becca.rothwell@gmail.com>
2023-10-25 16:27:41 +0100

    refactor: Simplified sortByAmountThenName to use Object.entries as variable not function, simplified return statement to use map-mremoved unnecessary variables, renamed transactionCount to transactionCountObj, and changed from let to const

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,22 +1,9 @@
-const transactionResults = [];
-
 function processTransactions(transactions) {
   if (transactions === undefined) {
     throw new Error("Undefined collection of transactions");
   }
 
-  /*let transactionCount = {};
-
-  const numberOfTransactions = transactions.length;
-
-  for (var i = 0; i < numberOfTransactions; i++) {
-    const transaction = transactions[i];
-    transactionCount[transaction]
-      ? (transactionCount[transaction] += 1)
-      : (transactionCount[transaction] = 1);
-  }*/
-
-  let transactionCount = transactions.reduce(
+  const transactionCountObj = transactions.reduce(
     (transactionsCount, transaction) => {
       return {
         ...transactionsCount,
@@ -26,34 +13,13 @@ function processTransactions(transactions) {
     {}
   );
 
-  transactionCount = sortByAmountThenName(transactionCount);
+  const sortByAmountThenName = Object.entries(transactionCountObj).sort(
+    (a, b) => {
+      return b[1] - a[1] || a[0] > b[0] || -(a[0] < b[0]);
+    }
+  );
 
-  // Place them back in array for returning
-  Object.keys(transactionCount).forEach(function (key, index) {
-    transactionResults[index] = `${key} ${transactionCount[key]}`;
-  });
-
-  return transactionResults;
-}
-
-function sortByAmountThenName(transactionCount) {
-  let sortedKeys = Object.keys(transactionCount).sort(function sortingFunction(
-    itemOne,
-    itemTwo
-  ) {
-    return (
-      transactionCount[itemTwo] - transactionCount[itemOne] ||
-      itemOne > itemTwo ||
-      -(itemOne < itemTwo)
-    );
-  });
-
-  let sortedResults = {};
-  for (let objectKey of sortedKeys) {
-    sortedResults[objectKey] = transactionCount[objectKey];
-  }
-
-  return sortedResults;
+  return sortByAmountThenName.map((arr) => arr.join(" "));
 }
 
 module.exports = processTransactions;
